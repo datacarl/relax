90f64ede396d24ce8c08d0bd2a2351953dda54d6
Carl Littke <carl.littke@gmail.com>
2013-11-16 19:52:56 +0100

    Add todos to items.

--- client/item.js
+++ client/item.js
@@ -17,8 +17,6 @@ Template.item.events({
       item[attr] = tmpl.find('#' + attr).value;
     });
 
-    console.log(item);
-
     if (data.item) {
       Items.update(data.item._id, {$set : item});
     } else {
@@ -26,4 +24,41 @@ Template.item.events({
       Router.go(Router.path('item', {_id: id}));
     }
   }
+});
+
+Template.todo.events({
+  'click .done': function(e, tmpl) {
+    // https://github.com/meteor/meteor/issues/153
+    e.preventDefault();
+    var item = Router.getData().item,
+        index = _.indexOf(item.todos, this),
+        modifier = {$set: {}};
+        modifier.$set["todos." + index + ".completedAt"] = new Date();
+        Items.update(item._id, modifier);
+  },
+  'submit form': function(e, tmpl) {
+    e.preventDefault();
+    var item = Router.getData().item;
+        index = _.indexOf(item.todos, this),
+        modifier = {$set: {}};
+        modifier.$set["todos." + index + ".task"] = tmpl.find('.task').value;
+        modifier.$set["todos." + index + ".due"] = tmpl.find('.due').value;
+        Items.update(item._id, modifier);
+    Items.update(item._id, modifier);
+  },
+});
+
+Template.newTodo.events({
+  'submit form': function(e, tmpl) {
+    e.preventDefault();
+    Items.update(Router.getData().item._id, {
+      $push : {
+        todos : {
+          task: tmpl.find('.task').value,
+          due: tmpl.find('.due').value,
+        }
+      }
+    });
+    Router.go(Router.path('item', Router.current().params));
+  },
 });
