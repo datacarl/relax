355076fa6c79ea9c13892d986a8711a037f5e0ab
Carl Littke <carl.littke@gmail.com>
2014-07-28 14:57:16 +0200

    Update meteor an iron router.

--- client/handlebarshelpers.js
+++ client/handlebarshelpers.js
@@ -1,8 +1,8 @@
-Handlebars.registerHelper('session', function(key) {
+UI.registerHelper('session', function(key) {
   return Session.get(key);
 });
 
-Handlebars.registerHelper('getDate', function(date) {
+UI.registerHelper('getDate', function(date) {
   if (date) {
     return "" + date.getUTCFullYear() + "-" +
           ("0" + (date.getMonth() + 1)).slice(-2) + "-" +
